35aa04e54fb419e925bdf7baf5003528b5c6bbaa
victorhoracio <victorhoraciollanos@gmail.com>
2025-08-12 17:03:32 -0300

    agregur las rutas al app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { BrowserRouter, Route, Routes } from "react-router";
 import Administrador from "./components/pages/Administrador";
 import DetalleProducto from "./components/pages/DetalleProducto";
 import Error404 from "./components/pages/Error404";
@@ -10,15 +11,22 @@ import Menu from "./components/shared/Menu";
 function App() {
   return (
     <>
+    <BrowserRouter>
       <Menu></Menu>
       <main>
-        {/* <Error404></Error404> */}
-        {/* <Inicio></Inicio> */}
-        {/* <Administrador></Administrador> */}
-        {/* <DetalleProducto></DetalleProducto> */}
-        <FormularioProducto></FormularioProducto>
+        <Routes>
+          <Route path="/" element={<Inicio></Inicio>}>Inicio</Route>
+           <Route path="/detalle" element={<DetalleProducto></DetalleProducto>}>Detalle</Route>
+           <Route path="/administrador" element={<Administrador></Administrador>}>Administrador</Route>           
+              <Route path="/administrador/crear" element={ <FormularioProducto></FormularioProducto>}>Crear</Route>
+                 <Route path="/administrador/editar" element={ <FormularioProducto></FormularioProducto>}>Editar</Route>
+                    <Route path="*" element={<Error404></Error404>}>Error</Route>
+
+        </Routes>
+             
       </main>
       <Footer></Footer>
+      </BrowserRouter>
     </>
   );
 }
